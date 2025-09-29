4a2b903e6343fbb35e46037d860c432b4d8796a3
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2025-09-30 00:46:17 +0200

    Crude setup

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList],
 })
 export class HomePage {
+  medications = [
+    { name: 'Aspirin', dosage: '100mg', time: '08:00' },
+    { name: 'Vitamin D', dosage: '1000 IU', time: '09:00' },
+    { name: 'Ibuprofen', dosage: '200mg', time: '20:00' }
+  ];
+
   constructor() {}
 }
