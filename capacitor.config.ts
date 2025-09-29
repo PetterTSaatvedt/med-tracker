bee1dbace0f7bdaa23b7c170f096b68ceda522fd
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2025-09-30 00:20:51 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'med-tracker',
+  webDir: 'www'
+};
+
+export default config;
