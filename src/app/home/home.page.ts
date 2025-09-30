0213d0a555220cb284a6a6671c703b0ea2aef635
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2025-09-30 11:50:26 +0200

    Button for adding medication to medication list

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,18 +1,30 @@
+import { Medication } from './../models/medication.model';
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon],
 })
 export class HomePage {
-  medications = [
-    { name: 'Aspirin', dosage: '100mg', time: '08:00' },
+
+  medications: Medication[] = [
+    { name: 'Aspirin', dosage: '100mg', time: '08:00', },
     { name: 'Vitamin D', dosage: '1000 IU', time: '09:00' },
     { name: 'Ibuprofen', dosage: '200mg', time: '20:00' }
   ];
 
-  constructor() {}
+  addMedication() {
+      this.medications.push(
+        { name: 'New Medicine', dosage: '50mg', time: '12:00'}
+      );
+  }
+
+  constructor() {
+    addIcons({addOutline})
+  }
 }
