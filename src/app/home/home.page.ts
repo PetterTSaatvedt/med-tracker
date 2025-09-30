90fdbd282f6a283f502d3d2c9bfb672eaccabdc7
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2025-10-01 01:10:38 +0200

    User can now delete medications

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,15 +1,15 @@
 import { Medication } from './../models/medication.model';
-import { Component, ViewChild } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon, IonModal, IonInput, IonButtons } from '@ionic/angular/standalone';
+import { Component, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon, IonModal, IonInput, IonButtons, IonAlert } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { addOutline } from 'ionicons/icons';
+import { addOutline, removeCircleOutline } from 'ionicons/icons';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon, IonModal, IonInput, IonButtons, FormsModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon, IonModal, IonInput, IonButtons, FormsModule, IonAlert],
 })
 export class HomePage {
 
@@ -19,9 +19,33 @@ export class HomePage {
     { name: 'Ibuprofen', dosage: '200mg', time: '20:00' }
   ];
 
+  // Form properties
   newMedName: string = '';
   newMedDosage: string = '';
   newMedTime: string = '';
+
+  // Delete properties
+  medicationToDelete: number | null = null;
+  deleteMedName: string = '';
+  @ViewChild('deleteAlert') deleteAlert!: IonAlert;
+
+  alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel'
+    },
+    {
+      text: 'Delete',
+      role: 'confirm',
+      handler: () => {
+        if (this.medicationToDelete !== null) {
+          this.deleteMedication(this.medicationToDelete);
+          this.medicationToDelete = null;
+          this.cdr.detectChanges(); // needed to immediately apply changes to the UI
+        }
+      }
+    }
+  ];
 
   @ViewChild('modal') modal!: IonModal;
 
@@ -48,7 +72,17 @@ export class HomePage {
     this.newMedTime = '';
   }
 
-  constructor() {
-    addIcons({addOutline})
+  confirmDelete(index: number, name: string) {
+    this.medicationToDelete = index;
+    this.deleteMedName = name;
+    this.deleteAlert.present();
+  }
+
+  deleteMedication(index: number) {
+    this.medications.splice(index, 1);
+  }
+
+  constructor(private cdr: ChangeDetectorRef) {
+    addIcons({addOutline, removeCircleOutline})
   }
 }
