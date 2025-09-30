cf8076af0439b50ceb89629130949107a7775ba5
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2025-09-30 17:21:33 +0200

    User can now add medications through modal

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,15 @@
 import { Medication } from './../models/medication.model';
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { Component, ViewChild } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon, IonModal, IonInput, IonButtons } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { addOutline } from 'ionicons/icons';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon, IonModal, IonInput, IonButtons, FormsModule],
 })
 export class HomePage {
 
@@ -18,10 +19,33 @@ export class HomePage {
     { name: 'Ibuprofen', dosage: '200mg', time: '20:00' }
   ];
 
+  newMedName: string = '';
+  newMedDosage: string = '';
+  newMedTime: string = '';
+
+  @ViewChild('modal') modal!: IonModal;
+
+  saveMedication() {
+    if (!this.newMedName || !this.newMedDosage || !this.newMedTime) {
+      alert("To save your medication, please fill all sections.")
+      return;
+    }
+
+    this.addMedication();
+    this.modal.dismiss();
+    this.clearForm();
+  }
+
   addMedication() {
-      this.medications.push(
-        { name: 'New Medicine', dosage: '50mg', time: '12:00'}
-      );
+    this.medications.push(
+      { name: this.newMedName, dosage: this.newMedDosage, time: this.newMedTime}
+    );
+  }
+
+  clearForm() {
+    this.newMedName = '';
+    this.newMedDosage = '';
+    this.newMedTime = '';
   }
 
   constructor() {
