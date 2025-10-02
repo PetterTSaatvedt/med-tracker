f7247cc3f413d6371b4d71c5d84355969929ed20
PetterTSaatvedt <71778703+PetterTSaatvedt@users.noreply.github.com>
2025-10-02 13:59:33 +0200

    Moved logic and template code for medications to correct files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,91 +1,14 @@
-import { Medication } from './../models/medication.model';
-import { Component, ViewChild, ChangeDetectorRef, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon, IonModal, IonInput, IonButtons, IonAlert } from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import { addOutline, removeCircleOutline } from 'ionicons/icons';
-import { FormsModule } from '@angular/forms';
-import { Storage } from '../services/storage';
+import { Component} from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon, IonModal, IonInput, IonButtons, FormsModule, IonAlert],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomePage implements OnInit {
+export class HomePage {
 
-  medications: Medication[] = [];
-
-  // Form properties
-  newMedName: string = '';
-  newMedDosage: string = '';
-  newMedTime: string = '';
-
-  // Delete properties
-  medicationToDelete: number | null = null;
-  deleteMedName: string = '';
-  @ViewChild('deleteAlert') deleteAlert!: IonAlert;
-
-  alertButtons = [
-    {
-      text: 'Cancel',
-      role: 'cancel'
-    },
-    {
-      text: 'Delete',
-      role: 'confirm',
-      handler: () => {
-        if (this.medicationToDelete !== null) {
-          this.deleteMedication(this.medicationToDelete);
-          this.medicationToDelete = null;
-          this.cdr.detectChanges();
-        }
-      }
-    }
-  ];
-
-  @ViewChild('modal') modal!: IonModal;
-
-  saveMedication() {
-    if (!this.newMedName || !this.newMedDosage || !this.newMedTime) {
-      alert("To save your medication, please fill all sections.")
-      return;
-    }
-
-    this.addMedication();
-    this.modal.dismiss();
-    this.clearForm();
-  }
-
-  addMedication() {
-    this.medications.push(
-      { name: this.newMedName, dosage: this.newMedDosage, time: this.newMedTime}
-    );
-    this.storage.saveMedications(this.medications);
-  }
-
-  deleteMedication(index: number) {
-    this.medications.splice(index, 1);
-    this.storage.saveMedications(this.medications);
-  }
-
-  clearForm() {
-    this.newMedName = '';
-    this.newMedDosage = '';
-    this.newMedTime = '';
-  }
-
-  confirmDelete(index: number, name: string) {
-    this.medicationToDelete = index;
-    this.deleteMedName = name;
-    this.deleteAlert.present();
-  }
-
-  constructor(private cdr: ChangeDetectorRef, private storage: Storage) {
-    addIcons({addOutline, removeCircleOutline});
-  }
-
-  async ngOnInit() {
-    this.medications = await this.storage.loadMedications();
+  constructor() {
   }
 }
--- src/app/medication-list/medication-list.page.ts
+++ src/app/medication-list/medication-list.page.ts
@@ -1,20 +1,91 @@
-import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Medication } from './../models/medication.model';
+import { Component, ViewChild, ChangeDetectorRef, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon, IonModal, IonInput, IonButtons, IonAlert } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { addOutline, removeCircleOutline } from 'ionicons/icons';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Storage } from '../services/storage';
 
 @Component({
   selector: 'app-medication-list',
-  templateUrl: './medication-list.page.html',
-  styleUrls: ['./medication-list.page.scss'],
-  standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  templateUrl: 'medication-list.page.html',
+  styleUrls: ['medication-list.page.scss'],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonList, IonButton, IonIcon, IonModal, IonInput, IonButtons, FormsModule, IonAlert],
 })
 export class MedicationListPage implements OnInit {
 
-  constructor() { }
+  medications: Medication[] = [];
 
-  ngOnInit() {
+  // Form properties
+  newMedName: string = '';
+  newMedDosage: string = '';
+  newMedTime: string = '';
+
+  // Delete properties
+  medicationToDelete: number | null = null;
+  deleteMedName: string = '';
+  @ViewChild('deleteAlert') deleteAlert!: IonAlert;
+
+  alertButtons = [
+    {
+      text: 'Cancel',
+      role: 'cancel'
+    },
+    {
+      text: 'Delete',
+      role: 'confirm',
+      handler: () => {
+        if (this.medicationToDelete !== null) {
+          this.deleteMedication(this.medicationToDelete);
+          this.medicationToDelete = null;
+          this.cdr.detectChanges();
+        }
+      }
+    }
+  ];
+
+  @ViewChild('modal') modal!: IonModal;
+
+  saveMedication() {
+    if (!this.newMedName || !this.newMedDosage || !this.newMedTime) {
+      alert("To save your medication, please fill all sections.")
+      return;
+    }
+
+    this.addMedication();
+    this.modal.dismiss();
+    this.clearForm();
   }
 
+  addMedication() {
+    this.medications.push(
+      { name: this.newMedName, dosage: this.newMedDosage, time: this.newMedTime}
+    );
+    this.storage.saveMedications(this.medications);
+  }
+
+  deleteMedication(index: number) {
+    this.medications.splice(index, 1);
+    this.storage.saveMedications(this.medications);
+  }
+
+  clearForm() {
+    this.newMedName = '';
+    this.newMedDosage = '';
+    this.newMedTime = '';
+  }
+
+  confirmDelete(index: number, name: string) {
+    this.medicationToDelete = index;
+    this.deleteMedName = name;
+    this.deleteAlert.present();
+  }
+
+  constructor(private cdr: ChangeDetectorRef, private storage: Storage) {
+    addIcons({addOutline, removeCircleOutline});
+  }
+
+  async ngOnInit() {
+    this.medications = await this.storage.loadMedications();
+  }
 }
